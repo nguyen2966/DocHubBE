7a3decab1a2be188a71408dd7c43b696a9aa0c37
Nguyen <nguyentoankhoa12@gmail.com>
2026-06-01 18:13:09 +0700

    Initialize dto files for login and register

--- src/modules-api/auth/dto/register.dto.ts
+++ src/modules-api/auth/dto/register.dto.ts
@@ -1,10 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
 
-export class LoginDto{
+export class RegisterDto{
   @ApiProperty({example: "nguyen@gmail.com"})
   email: string;
 
   @ApiProperty({example: "123456"})
   password: string;
+
+  @ApiProperty({example:"123456"})
+  repear_password: string;
 
 }
