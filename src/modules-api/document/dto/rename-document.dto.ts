ee925a61a7a5b2c5951ad5a147eed82aec925ffb
Nguyen <nguyentoankhoa12@gmail.com>
2026-06-21 20:53:50 +0700

    [Refactor] serialize comments

--- src/modules-api/document/dto/rename-document.dto.ts
+++ src/modules-api/document/dto/rename-document.dto.ts
@@ -1,8 +1,10 @@
-import {IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class RenameDocumentDto {
-  @ApiProperty()
+  @ApiProperty({ maxLength: 255 })
   @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
   title: string
 }
