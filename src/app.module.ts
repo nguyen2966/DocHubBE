1a0f84bb0069ae5a7d63705d0ad29ff4c64fa11c
Nguyen <nguyentoankhoa12@gmail.com>
2026-06-24 20:45:53 +0700

    [Refactor] remove redundant module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { AuthModule } from './modules-api/auth/auth.module';
 import { WorkspaceModule } from './modules-api/workspace/workspace.module';
 import { DocumentModule } from './modules-api/document/document.module';
 import { CommentModule } from './modules-api/comment/comment.module';
-import { UploadModule } from './modules-api/upload/upload.module';
 import { ActivityModule } from './modules-api/activity/activity.module';
 import { SearchModule } from './modules-api/search/search.module';
 import { TokenModule } from './modules-system/token/token.module';
@@ -19,7 +18,6 @@ import { ProtectGuard } from './common/guards/protect.guard';
             WorkspaceModule, 
             DocumentModule, 
             CommentModule, 
-            UploadModule, 
             ActivityModule, 
             SearchModule, 
             TokenModule,
